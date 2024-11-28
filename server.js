7f69e20cb0cb4c5bbf2ccc3a65e19ae534c740b2
MrRox1337 <amanrox97@gmail.com>
2024-11-28 22:36:57 +0400

    Minor bugfixes

--- server.js
+++ server.js
@@ -86,7 +86,7 @@ function deleteObject(req, res, next) {
 }
 
 function searchObject(req, res, next) {
-	const searchQuery = req.query || "";
+	const searchQuery = req.query.query || "";
 	console.log("query: ", searchQuery);
 	// Search in multiple fields: subject, spaces, location, and price
 	req.collection
